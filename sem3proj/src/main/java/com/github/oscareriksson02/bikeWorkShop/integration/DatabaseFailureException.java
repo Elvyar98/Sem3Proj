d9118d86cbbc64687bd7d1722e4c5311535011b6
Elvis Yarquah <elvispk35@hotmail.com>
2026-05-14 09:15:38 +0200

    Write CustomerNotFoundException Class which extends Exception, write DatabaseFailureException and SystemFailureException. Both extend runtimeException. Write contructor for all 3 classes.

---
+++ sem3proj/src/main/java/com/github/oscareriksson02/bikeWorkShop/integration/DatabaseFailureException.java
@@ -0,0 +1,11 @@
+package com.github.oscareriksson02.bikeWorkShop.integration;
+
+/**
+ * DatabaseFailureException class is thrown when
+ * the database is unreacble for any reason
+ */
+public class DatabaseFailureException extends RuntimeException {
+    
+
+
+}
