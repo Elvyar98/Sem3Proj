d8aa625669729fd7cb7e214f9706156ae346e351
Elvis Yarquah <elvispk35@hotmail.com>
2026-05-15 12:05:25 +0200

    Create observer objects in main and add them to controller

--- sem3proj/src/main/java/com/github/oscareriksson02/bikeWorkShop/startup/Main.java
+++ sem3proj/src/main/java/com/github/oscareriksson02/bikeWorkShop/startup/Main.java
@@ -1,8 +1,10 @@
 package com.github.oscareriksson02.bikeWorkShop.startup;
 
 import com.github.oscareriksson02.bikeWorkShop.integration.RegistryCreator;
+import com.github.oscareriksson02.bikeWorkShop.integration.RepairOrderLogger;
 import com.github.oscareriksson02.bikeWorkShop.model.OrderState;
 import com.github.oscareriksson02.bikeWorkShop.controller.Controller;
+import com.github.oscareriksson02.bikeWorkShop.view.RepairOrderView;
 import com.github.oscareriksson02.bikeWorkShop.view.View;
 import com.github.oscareriksson02.bikeWorkShop.integration.Printer;
 import com.github.oscareriksson02.bikeWorkShop.integration.FileLogger;
@@ -22,6 +24,10 @@ public class Main {
         FileLogger filelogger = new FileLogger();
         Controller contr = new Controller(creator, printer, filelogger);
         View view = new View(contr);
+        RepairOrderView repairOrderView = new RepairOrderView();
+        RepairOrderLogger repairOrderLogger = new RepairOrderLogger();
+        contr.addObserver(repairOrderLogger);
+        contr.addObserver(repairOrderView);
 
         view.searchCustomer("0701234567");
 
